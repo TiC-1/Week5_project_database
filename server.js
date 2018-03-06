e088dd2e13694b3eeda9bdd1864416b143cba7f7
Iannis Camillieri <ianniscamillieri@gmail.com>
2018-03-06 12:25:56 +0100

    add some files for back-end development

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require("http");
+var router = require("./src/router.js");
+
+var server = http.createServer(router);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function() {
+  console.log("Listening on port: " + port);
+});
---
+++ src/router.js
@@ -0,0 +1,25 @@
+var http = require("http");
+var assetsHandler = require("./assetsHandler.js");
+var assetsHandler = require("./tasksHandler.js");
+var usersHandler = require("./usersHandler.js");
+
+function router(request, response) {
+  var endpoint = request.url;
+
+  if (endpoint === "/") {
+    assetsHandler.index(request, response);
+  } else if (endpoint === "/tasks") {
+    tasksHandler.index(request, response);
+  } else if (endpoint === "/users") {
+    usersHandler.index(request, response);
+  } else if (endpoint === "/task/create") {
+    tasksHandler.create(request, response);
+  } else if (endpoint === "/task/delete") {
+    tasksHandler.delete(request, response);
+
+  } else {
+    assetsHandler.static(request, response);
+  }
+};
+
+module.exports = router;
