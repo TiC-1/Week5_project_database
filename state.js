c1401b36f58742793368861e2b07879b0abea725
Iannis Camillieri <ianniscamillieri@gmail.com>
2018-03-07 15:21:47 +0100

    improved state.js

--- state.js
+++ state.js
@@ -1,8 +1,33 @@
 var state = [
-  { id: -3, description: 'first todo', assign: ['Al','Claudio','Matte', 'Iannis'] , done: false },
-  { id: -2, description: 'second todo', assign: ['Al','Claudio','Matte', 'Iannis'], done: false },
-  { id: -1, description: 'third todo', assign: ['Al','Claudio','Matte', 'Iannis'], done: false },
-];
+
+  {
+    id: -3,
+    title: 'first todo',
+    assign: [
+      {id: 1, name: 'Al'},
+      {id: 2, name: 'Claudio'},
+      {id: 3, name: 'Matte'},
+      {id: 4, name: 'Iannis'}
+      ]
+  },
+
+  {
+    id: -2,
+    title: 'second todo',
+    assign: [
+      {id: 2, name: 'Claudio'},
+      {id: 4, name: 'Iannis'}
+      ]
+  },
+
+  {
+    id: -1,
+    title: 'third todo',
+    assign: [
+      {id: 3, name: 'Matte'}
+      ]
+  }
+]
 
 if (typeof module !== 'undefined') {
   module.exports = state;
