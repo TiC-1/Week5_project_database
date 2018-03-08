691cac5ed9efece6a41c6c10376bf045c9bb2770
Iannis Camillieri <ianniscamillieri@gmail.com>
2018-03-08 10:55:50 +0100

    change whole assigned tasks list query

--- src/tasksHandler.js
+++ src/tasksHandler.js
@@ -2,7 +2,7 @@ var db = require("./database/db_connection.js");
 var querystring = require("querystring");
 
 function index(request, response) {
-  db.query("SELECT * FROM tasks", function(err, result) {
+  db.query("SELECT *, (SELECT json_agg(users.*) FROM tasks_assignments, users WHERE tasks.id=task_id AND user_id=users.id) AS assign FROM tasks;", function(err, result) {
     if(err) {
       response.writeHead(500);
       return response.end();
@@ -12,20 +12,6 @@ function index(request, response) {
     });
     response.end(JSON.stringify(result.rows));
   });
-}
-
-function index2(request, response) {
-  db.query("SELECT ta.task_id AS id, t.title, ta.user_id FROM users AS u, tasks AS t, tasks_assignments AS ta WHERE u.id = ta.user_id AND t.id = ta.task_id;", function(err, result) {
-    if (err) {
-      response.writeHead(500);
-      return response.end();
-    }
-    response.writeHead(200, {
-      "Content-Type": "application/json"
-    });
-    response.end(JSON.stringify(result.rows));
-  });
-  cb(JSON.stringify(result.rows));
 }
 
 function create(request, response) {
@@ -66,5 +52,4 @@ module.exports = {
   index: index,
   create: create,
   deleteTask: deleteTask,
-  index2: index2
 };
--- test/tasks.test.js
+++ test/tasks.test.js
@@ -10,7 +10,7 @@ test("Populate the database", function(assert) {
   });
 });
 
-test("Returns tasks in array and check property title", function(assert) {
+test("Returns tasks list in array and check for properties", function(assert) {
   supertest(tasksHandler.index)
     .get("/tasks")
     .end(function(err, result) {
@@ -20,8 +20,9 @@ test("Returns tasks in array and check property title", function(assert) {
       }
       var parsedResult = JSON.parse(result.text);
       console.log(parsedResult);
-      assert.ok(parsedResult.length > 0, "has items");
-      assert.ok(parsedResult[0].hasOwnProperty("title"), "item has title");
+      assert.ok(parsedResult.length > 0, "list has items");
+      assert.ok(parsedResult[0].hasOwnProperty("assign"), "item has assign");
+      assert.ok(parsedResult[0].assign[0].hasOwnProperty("username"), "assign has username");
       assert.end();
     });
 });
@@ -58,63 +59,6 @@ test("Delete a task", function(assert) {
     });
 });
 
-test("Build assigned tasks list", function(assert) {
-  var queryResult = [
-    {id: 1, title: 'Come home from work singing', user_id: 2},
-    {id: 1, title: 'Come home from work singing', user_id: 1},
-    {id: 2, title: 'Wake up Snow White', user_id: 4},
-    {id: 3, title: 'Throw apples out in the trash!', user_id: 3},
-    {id: 3, title: 'Throw apples out in the trash!', user_id: 1},
-    {id: 3, title: 'Throw apples out in the trash!', user_id: 2},
-    {id: 4, title: 'Call Prince Charming', user_id: 2},
-    {id: 4, title: 'Call Prince Charming', user_id: 4},
-  ]
-
-  assignedTasksList(queryResult);
-  assert.ok("list has 4 tasks");
-  assert.end();
-});
-
-function assignedTasksList(arr) { // input will be query response
-  var tasksIndex = []; // final whole object
-  var obj = {}; // task temp object
-  var assign = []; // array of assignees
-  console.log("** INPUT **");
-  console.log(arr);
-  var i = 0;
-  var j = 0;
-
-  if (arr.length > 0) {
-    do {
-      console.log(" ");
-      console.log("enter i loop => i =", i, ". j =", j);
-      console.log("building task object...");
-      obj = {};
-      obj.id = arr[i].id;
-      obj.title = arr[i].title;
-      obj.assign = [];
-
-      j = i;
-      do {
-        console.log("enter j loop => i =", i, ". j =", j);
-        console.log("building assignees array...");
-        obj['assign'].push(arr[j].user_id);
-        j++;
-      }
-      while (j < arr.length && arr[j].id == arr[i].id);
-
-      console.log(" ");
-      console.log("pushing", obj, "to tasks list");
-      tasksIndex.push(obj);
-      i = j;
-    }
-    while (i < arr.length);
-  }
-
-  console.log(" ");
-  console.log("** OUTPUT **");
-  console.log(tasksIndex);
-}
 
 // test("End pool connection", function(assert) {
 //   db.end(function() {
